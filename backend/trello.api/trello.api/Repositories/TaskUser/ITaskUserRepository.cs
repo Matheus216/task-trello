49f270b2f2c198811cfeb952d76bef0af69d2ce8
Matheus216 <Matheusmartinsss216@gmail.com>
2020-09-23 21:00:47 -0300

    Add rules repositories to taskUser

--- backend/trello.api/trello.api/Repositories/TaskUser/ITaskUserRepository.cs
+++ backend/trello.api/trello.api/Repositories/TaskUser/ITaskUserRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using trello.api.Repositories.Entities.Models;
 
 namespace trello.api.Repositories.TaskUser
@@ -5,5 +6,7 @@ namespace trello.api.Repositories.TaskUser
     public interface ITaskUserRepository : IRepository<TaskUserEntityModel>
     {
         TaskUserEntityModel GetById(int taskId, int userId);
+        IList<TaskUserEntityModel> GetByTaskId(int taskId);
+        IList<TaskUserEntityModel> GetByUserId(int userId);
     }
 }
--- backend/trello.api/trello.api/Repositories/TaskUser/TaskUserRepository.cs
+++ backend/trello.api/trello.api/Repositories/TaskUser/TaskUserRepository.cs
@@ -1,33 +1,82 @@
 using System.Collections.Generic;
+using System.Linq;
+using trello.api.Repositories.Entities.Context;
 using trello.api.Repositories.Entities.Models;
 
 namespace trello.api.Repositories.TaskUser
 {
     public class TaskUserRepository : ITaskUserRepository
     {
+        private ContextDB _context;
+        public TaskUserRepository()
+        {
+            _context = new ContextDB();
+        }
+
         public void Delete(int id)
         {
-            throw new System.NotImplementedException();
+            var searchObj = GetById(id);
+
+            _context.TaskUser.Remove(searchObj);
+            _context.SaveChanges();
         }
 
         public List<TaskUserEntityModel> GetAll()
         {
-            throw new System.NotImplementedException();
-        }
+            var _search = _context.TaskUser
+                .ToList();
 
-        public TaskUserEntityModel GetById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+            if (_search == null)
+            {
+                _search = new List<TaskUserEntityModel>();
+            }
 
+            return _search;
+        }
         public TaskUserEntityModel Insert(TaskUserEntityModel objIn)
         {
-            throw new System.NotImplementedException();
+            var insered = _context.TaskUser.Add(objIn);
+            _context.SaveChanges();
+
+            return insered.Entity;
         }
 
         public TaskUserEntityModel Update(TaskUserEntityModel objIn)
         {
+            var updated = _context.TaskUser.Update(objIn);
+
+            _context.SaveChanges();
+
+            return updated.Entity;
+        }
+        public TaskUserEntityModel GetById(int taskId, int userId)
+        {
+            var searh = _context.TaskUser?.FirstOrDefault(x => x.TaskId == taskId && x.UserId == userId); 
+
+            return searh; 
+        }
+        
+        public TaskUserEntityModel GetById(int taskId)
+        {
             throw new System.NotImplementedException();
+        }
+
+        public IList<TaskUserEntityModel> GetByTaskId(int taskId)
+        {
+            var _search = _context.TaskUser?
+                .Where(x => x.TaskId == taskId)
+                .ToList(); 
+
+            return _search; 
+        }
+
+        public IList<TaskUserEntityModel> GetByUserId(int userId)
+        {
+            var _search = _context.TaskUser?
+                .Where(x => x.UserId == userId)
+                .ToList(); 
+
+            return _search; 
         }
     }
 }
